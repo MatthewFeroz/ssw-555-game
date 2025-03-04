3ab32f74feb88494dafad2cff26b4c93ed5f59df
janamucci18 <janamucci18@gmail.com>
2025-03-04 16:51:05 -0500

    Added space background and planets for map and lesson 1 pretty much complete

---
+++ planet2_button.gd
@@ -0,0 +1,8 @@
+extends Button
+
+
+@export var lesson_scene: PackedScene  # Drag your lesson scene here in the Inspector
+
+func _on_pressed():
+	if lesson_scene:
+		get_tree().change_scene_to_packed(lesson_scene)  # Change to the lesson scene
