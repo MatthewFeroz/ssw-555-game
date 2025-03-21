1f37dfc8ac554fe9c495e5f8e77f4d8f016c4ec2
cassiama <chanceyassiamah@gmail.com>
2025-03-20 22:28:05 -0400

    refactored tetrimino scene to act more like a tool (will draw the selected shape when scene is reloaded)

--- scripts/tetrimino.gd
+++ scripts/tetrimino.gd
@@ -1,9 +1,10 @@
 @tool
 extends Node2D
 
-@export var color: Color = Color.WHITE
-enum SHAPE {O, I, T, L, J, S, Z}
-@export var tetrimino_shape = SHAPE
+enum Shape {O, I, T, L, J, S, Z}
+@export var tetrimino_shape = Shape.O:
+	set(new_shape):
+		tetrimino_shape = new_shape
 
 
 # original DS screen resolution = 256x192 (per screen)
@@ -23,176 +24,195 @@ enum SHAPE {O, I, T, L, J, S, Z}
 # O-Shape
 """
 [
-    (0,0),
-    (1,0),
-    (0,1),
-    (1,0),
+	(0,0),
+	(1,0),
+	(0,1),
+	(1,0),
 ]
 """
 var O_SHAPE = {
-    "coords": [
-        [0.0, 0.0],
-        [0.0, 1.0],
-        [1.0, 0.0],
-        [1.0, 1.0],
-    ],
-    "color": Color.YELLOW,
+	"coords": [
+		[0.0, 0.0],
+		[0.0, 1.0],
+		[1.0, 0.0],
+		[1.0, 1.0],
+	],
+	"color": Color.YELLOW,
 }
 
 # I-Shape
 """
 [
-    (0,0),
-    (0,1),
-    (0,2),
-    (0,3).
+	(0,0),
+	(0,1),
+	(0,2),
+	(0,3).
 ]
 """
 var I_SHAPE = {
-    "coords": [
-        [0.0, 0.0],
-        [0.0, 1.0],
-        [0.0, 2.0],
-        [0.0, 3.0],
-    ],
-    "color": Color.CYAN
+	"coords": [
+		[0.0, 0.0],
+		[0.0, 1.0],
+		[0.0, 2.0],
+		[0.0, 3.0],
+	],
+	"color": Color.CYAN
 }
 
 # T-Shape
 """
 [
-    (1,0),
-    (0,1),
-    (1,1),
-    (2,1),
+	(1,0),
+	(0,1),
+	(1,1),
+	(2,1),
 ]
 """
 var T_SHAPE = {
-    "coords": [
-        [1.0, 0.0],
-        [0.0, 1.0],
-        [1.0, 1.0],
-        [2.0, 1.0],
-    ],
-    "color": Color.PURPLE
+	"coords": [
+		[1.0, 0.0],
+		[0.0, 1.0],
+		[1.0, 1.0],
+		[2.0, 1.0],
+	],
+	"color": Color.PURPLE
 }
 # L-Shape
 """
 [
-    (2,0),
-    (0,1),
-    (1,1),
-    (2,1),
+	(2,0),
+	(0,1),
+	(1,1),
+	(2,1),
 ]
 """
 var L_SHAPE = {
-    "coords": [
-        [2.0, 0.0],
-        [0.0, 1.0],
-        [1.0, 1.0],
-        [2.0, 1.0],
-    ],
-    "color": Color.ORANGE
+	"coords": [
+		[2.0, 0.0],
+		[0.0, 1.0],
+		[1.0, 1.0],
+		[2.0, 1.0],
+	],
+	"color": Color.ORANGE
 }
 
 # J-Shape
 """
 [
-    (0,0),
-    (0,1),
-    (1,1),
-    (2,1),
+	(0,0),
+	(0,1),
+	(1,1),
+	(2,1),
 ]
 """
 var J_SHAPE = {
-    "coords": [
-        [0.0, 0.0],
-        [0.0, 1.0],
-        [1.0, 1.0],
-        [2.0, 1.0],
-    ],
-    "color": Color.BLUE
+	"coords": [
+		[0.0, 0.0],
+		[0.0, 1.0],
+		[1.0, 1.0],
+		[2.0, 1.0],
+	],
+	"color": Color.BLUE
 }
 
 # S-Shape
 """
 [
-    (1,0),
-    (2,0),
-    (1,0),
-    (1,1),
+	(1,0),
+	(2,0),
+	(1,0),
+	(1,1),
 ]
 """
 var S_SHAPE = {
-    "coords": [
-        [1.0, 0.0],
-        [2.0, 0.0],
-        [0.0, 1.0],
-        [1.0, 1.0],
-    ],
-    "color": Color.LIME
+	"coords": [
+		[1.0, 0.0],
+		[2.0, 0.0],
+		[0.0, 1.0],
+		[1.0, 1.0],
+	],
+	"color": Color.LIME
 }
 
 # Z-Shape
 """
 [
-    (0,0),
-    (1,0),
-    (1,1),
-    (2,1),
+	(0,0),
+	(1,0),
+	(1,1),
+	(2,1),
 ]
 """
 var Z_SHAPE = {
-    "coords": [
-        [0.0, 0.0],
-        [1.0, 0.0],
-        [1.0, 1.0],
-        [2.0, 1.0],
-    ],
-    "color": Color.RED
+	"coords": [
+		[0.0, 0.0],
+		[1.0, 0.0],
+		[1.0, 1.0],
+		[2.0, 1.0],
+	],
+	"color": Color.RED
 }
 
 func convert_coords_to_rect(coords, block_scale) -> Array[Rect2]:
-    var rects: Array[Rect2] = []
-    for coord in coords:
-        var rect = Rect2(
-            coord[0] * block_scale,
-            coord[1] * block_scale,
-            block_scale,
-            block_scale
-        )
+	var rects: Array[Rect2] = []
+	for coord in coords:
+		var rect = Rect2(
+			coord[0] * block_scale,
+			coord[1] * block_scale,
+			block_scale,
+			block_scale
+		)
 
-        rects.append(rect)
-    return rects
+		rects.append(rect)
+	return rects
 
 func draw_shape(
-    rects: Array[Rect2],
-    starting_coords: Vector2,
-    block_color: Color
+	rects: Array[Rect2],
+	starting_coords: Vector2,
+	block_color: Color
 ) -> void:
-    for rect in rects:
-        # print(rect)
-        rect.position += starting_coords
-        draw_rect(rect, block_color)
+	for rect in rects:
+		# print(rect)
+		rect.position += starting_coords
+		draw_rect(rect, block_color)
 
 
 func _draw() -> void:
-    # all of this code is to simply test drawing tetriminos to screen
-    var shapes = [
-        O_SHAPE,
-        I_SHAPE,
-        T_SHAPE,
-        L_SHAPE,
-        J_SHAPE,
-        S_SHAPE,
-        Z_SHAPE,
-    ]
-    var block_scale = 80.0 # scale up block by 80 units
-    var starting_coords = Vector2(0.0, 0.0)
+	var block_scale = 80.0 # scale up block by 80 units
+	var shape
+	match tetrimino_shape:
+		Shape.O:
+			shape = O_SHAPE
+		Shape.I:
+			shape = I_SHAPE
+		Shape.T:
+			shape = T_SHAPE
+		Shape.L:
+			shape = L_SHAPE
+		Shape.J:
+			shape = J_SHAPE
+		Shape.S:
+			shape = S_SHAPE
+		Shape.Z:
+			shape = Z_SHAPE
+	var rects = convert_coords_to_rect(shape["coords"], block_scale)
+	draw_shape(rects, Vector2(0.0, 0.0), shape["color"])
 
-    for shape in shapes:
-        var rects = convert_coords_to_rect(shape["coords"], block_scale)
-        # print(rects)
-        draw_shape(rects, starting_coords, shape["color"])
-        # TODO: figure out a good distance between shapes (for testing purposes)
-        starting_coords.x += 250
+	# all of this code below is to simply test drawing tetriminos to screen
+	# var shapes = [
+	#     O_SHAPE,
+	#     I_SHAPE,
+	#     T_SHAPE,
+	#     L_SHAPE,
+	#     J_SHAPE,
+	#     S_SHAPE,
+	#     Z_SHAPE,
+	# ]
+	# var starting_coords = Vector2(0.0, 0.0)
+
+	# for shape in shapes:
+	#     var rects = convert_coords_to_rect(shape["coords"], block_scale)
+	#     # print(rects)
+	#     draw_shape(rects, starting_coords, shape["color"])
+	#     # TODO: figure out a good distance between shapes (for testing purposes)
+	#     starting_coords.x += 250
