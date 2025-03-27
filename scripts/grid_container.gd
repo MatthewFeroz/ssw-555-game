62a3c458389d4842495ede25b983e32e89a34b91
cassiama <chanceyassiamah@gmail.com>
2025-03-26 21:43:12 -0400

    changed grid to be made up of ColoredRects instead of Block scenes

--- scripts/grid_container.gd
+++ scripts/grid_container.gd
@@ -1,11 +1,9 @@
 extends Node2D
 
 # constants
-const BLOCK_SCENE_PATH = "res://scenes/block.tscn"
-const BLOCK_SCENE = preload(BLOCK_SCENE_PATH)
 const BACKGROUND_COLOR = Color.DARK_GRAY
 const BORDER_COLOR = Color.DIM_GRAY
-const BLOCK_SIZE = 32.0	# all blocks must be 32x32
+const BLOCK_SIZE = Vector2(32.0, 32.0)	# all blocks must be 32x32
 
 # instance variables
 var grid: Node2D
@@ -20,35 +18,44 @@ func _ready() -> void:
 	grid.name = "Grid"
 	add_child(grid)
 	
+func _draw() -> void:
 	initialize_grid_border()
 	initialize_grid_background()
 	
 	
 func place_block(
+	container: Node2D,
 	block_num: int,
-	block_name,
+	block_name: String,
 	pos: Vector2,
-	block_size: float,
+	block_size: Vector2,
 	color: Color,
-	remove_collision: bool = false
+	#remove_collision: bool = false
 ) -> void:
 	# create the block
-	var block = BLOCK_SCENE.instantiate()
+	#print("Position of block: " + str(pos))
+	var block = ColorRect.new()
 	block.name = block_name % block_num
+	#var block = BLOCK_SCENE.instantiate()
+	#block_container.name = block_name % block_num
 	
 	# resize it and give it a grey color
-	block.resize_block(block_size)
-	block.set_block_color(color)
+	block.size = block_size
+	block.modulate = color
+	#block_container.resize_block(block_size)
+	#block_container.set_block_color(color)
 	
 	# add the block and position it correctly
-	grid.add_child(block)
-	if remove_collision:
-		block.remove_block_collision()	# we shouldn't have collision for the background blocks
-	block.position = pos * block_size
-	block_num += 1
+	container.add_child(block)
+	block.position = pos * block_size.x
+	#grid.add_child(block_container)
+	#block_container.add_child(block)
+	#block.position = pos * block_size
 
 func initialize_grid_border() -> void:
 	var block_num = 1
+	var block_container = Node2D.new()
+	block_container.name = "GridBorder"
 	var sides = {
 		"top": {
 			"x": [-1, grid_width + 1],
@@ -70,26 +77,16 @@ func initialize_grid_border() -> void:
 	
 	for side in sides:
 		var pos: Vector2
-		print("Current side: " + side)
+		var side_container = Node2D.new()
+		side_container.name = "Border%s" % side.capitalize()
+		#print("Current side: " + side)
 		match side:
 			"top", "bottom":
 				for i in range(sides[side]["x"][0], sides[side]["x"][1]):
 					pos = Vector2(i + starting_pos, sides[side]["y"])
 					#print("Placing block at current position: " + str(pos))
 					place_block(
-						block_num,
-						"BorderBlock%d",
-						pos,
-						BLOCK_SIZE,
-						BORDER_COLOR
-					)
-					block_num += 1
-					
-			"left", "right":
-				for i in range(sides[side]["y"][0], sides[side]["y"][1]):
-					pos = Vector2(sides[side]["x"] + starting_pos, i + starting_pos)
-					#print("Placing block at current position: " + str(pos))
-					place_block(
+						side_container,
 						block_num,
 						"BorderBlock%d",
 						pos,
@@ -98,15 +95,42 @@ func initialize_grid_border() -> void:
 					)
 					block_num += 1
 
+			"left", "right":
+				for i in range(sides[side]["y"][0], sides[side]["y"][1]):
+					pos = Vector2(sides[side]["x"] + starting_pos, i + starting_pos)
+					#print("Placing block at current position: " + str(pos))
+					place_block(
+						side_container,
+						block_num,
+						"BorderBlock%d",
+						pos,
+						BLOCK_SIZE,
+						BORDER_COLOR
+					)
+					block_num += 1
+					
+		block_container.add_child(side_container)
+	grid.add_child(block_container)
+
 func initialize_grid_background() -> void:
 	var block_num = 1
+	var block_container = Node2D.new()
+	block_container.name = "GridBackground"
 	for x in range(grid_width):
+		var grid_row = Node2D.new()
+		grid_row.name = "GridRow%d" % (x + 1)
 		for y in range(grid_height):
+			var pos = Vector2(x + starting_pos, y + starting_pos)
+			#print("initialize_grid_background: Placing block at current position: " + str(pos))
 			place_block(
+				grid_row,
 				block_num,
 				"BGBlock%d",
-				Vector2(x + starting_pos, y + starting_pos),
+				pos,
 				BLOCK_SIZE,
 				BACKGROUND_COLOR,
-				true
+				#true
 			)
+			block_num += 1
+		block_container.add_child(grid_row)
+	grid.add_child(block_container)
