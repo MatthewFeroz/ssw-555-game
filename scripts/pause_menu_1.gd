ec25b227c2fa36d7c436776d316218b2f8e72487
vishnukanchi9 <vishnukanchi9@gmail.com>
2025-04-02 18:24:08 -0400

    Add files via upload

--- scripts/pause_menu_1.gd
+++ scripts/pause_menu_1.gd
@@ -16,9 +16,11 @@ func _on_resume_pressed() -> void:
 
 
 func _on_restart_pressed() -> void:
+	get_tree().paused = false
 	get_tree().reload_current_scene()
 
 
 func _on_exit_pressed() -> void:
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 	
