b56746827ea6bf84a56b63a63ce004843894f248
janamucci18 <janamucci18@gmail.com>
2025-04-09 11:05:25 -0400

    Refactored button scripts into parent node and deduped logic

---
+++ scripts/main_scene.gd
@@ -0,0 +1,26 @@
+extends Control
+
+@export var lesson_scene_1: PackedScene
+@export var lesson_scene_2: PackedScene
+
+@export var button1: Button
+@export var button2: Button
+
+func _ready():
+	button1.connect("pressed", Callable(self, "_on_button1_pressed"))
+	button2.connect("pressed", Callable(self, "_on_button2_pressed"))
+
+# Refactored handler with zero duplication
+func _change_to_lesson(scene: PackedScene, button_name: String):
+	if scene:
+		print("%s: Successfully assigned scene" % button_name)
+		get_tree().change_scene_to_packed(scene)
+	else:
+		print("❌ %s: Scene not assigned." % button_name)
+
+
+func _on_button1_pressed():
+	_change_to_lesson(lesson_scene_1, "Button 1")
+
+func _on_button2_pressed():
+	_change_to_lesson(lesson_scene_2, "Button 2")
