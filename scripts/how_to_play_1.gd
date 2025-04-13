3bf4f3ed3c36abed7151bdaa6dabcb3a6aa2b62b
janamucci18 <janamucci18@gmail.com>
2025-04-13 17:47:32 -0400

    Minor changes/debugging

--- scripts/how_to_play_1.gd
+++ scripts/how_to_play_1.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 
-func _on_next_button_pressed(): 
-	get_parent().visible = false  
+func _on_next_button_pressed():  
 	SaveGlobal.save_progress("res://scenes/mini_game_1.tscn")
 	get_tree().change_scene_to_file("res://scenes/mini_game_1.tscn")
