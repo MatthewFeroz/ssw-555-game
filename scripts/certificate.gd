44ca43bda2aa174b170ec86e949da2055043e5e0
cassiama <chanceyassiamah@gmail.com>
2025-04-16 18:14:17 -0400

    moved all .gd files into the correct location. also fixed bugs in the certificate scene (it runs now).

--- scripts/certificate.gd
+++ scripts/certificate.gd
@@ -1,9 +1,9 @@
-extends Control
+extends Node2D
 
 @export var player_name: String = ""
 
-@onready var name_label = $NameLabel
-@onready var date_label = $DateLabel
+@onready var name_label = $Control/Name
+@onready var date_label = $Control/Date
 
 func _ready():
 	name_label.text = player_name
