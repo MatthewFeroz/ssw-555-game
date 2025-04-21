e979236686389ba36d303e40bdf6d7d0ceaa0729
MatthewFeroz <matthewferoz@gmail.com>
2025-04-21 15:40:16 -0400

    Set stage more appropriately for coin collecting, added increasing variable for when coins are being collected

--- scripts/coin_base.gd
+++ scripts/coin_base.gd
@@ -2,7 +2,12 @@ extends Area2D
 
 signal coin_collected
 
+@onready var sound_player = $AudioStreamPlayer2D
 
 func _on_body_entered(_body: Node2D) -> void:
-	coin_collected.emit()
-	print ("Player Entered")
+	if _body.name == "Character":
+		_body.score += 1
+		sound_player.play()  # Play the sound!
+		self.queue_free()
+		print(_body.score)
+		
--- scripts/main_scene_1.gd
+++ scripts/main_scene_1.gd
@@ -6,9 +6,3 @@ func _ready():
 	# Fade in the scene by animating the alpha of the ColorRect
 	var tween = get_tree().create_tween()
 	tween.tween_property(color_rect, "modulate:a", 0.0, 1.5)  # Fade out the black screen in 1.5 seconds
-
-var coins_collected = 0
-
-
-func _on_coin_base_coin_collected() -> void:
-	print("Coin collected")
