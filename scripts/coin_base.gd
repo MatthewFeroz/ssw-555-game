7b3bcabb4f018b93b610d96fcfbcf9fc9cea2457
MatthewFeroz <matthewferoz@gmail.com>
2025-04-30 18:37:08 -0400

    Final changes to main scene and coin counting

--- scripts/coin_base.gd
+++ scripts/coin_base.gd
@@ -1,13 +1,10 @@
+# Coin.gd
 extends Area2D
 
 signal coin_collected
 
-@onready var sound_player = $AudioStreamPlayer2D
-
 func _on_body_entered(_body: Node2D) -> void:
 	if _body.name == "Character":
 		_body.score += 1
-		sound_player.play()  # Play the sound!
 		self.queue_free()
 		print(_body.score)
-		
